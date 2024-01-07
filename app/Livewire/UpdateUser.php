91e788dab7936a6dc731ebabe53b5f6778979a0b
Jonathan Zarate <zaratedev@gmail.com>
2024-01-07 16:05:21 -0600

    Edit name

--- app/Livewire/UpdateUser.php
+++ app/Livewire/UpdateUser.php
@@ -2,10 +2,60 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class UpdateUser extends Component
 {
+    public $user;
+    public $name;
+    public $email;
+
+    public $toggleName = false;
+
+    /**
+     * @var array
+     */
+    protected $rules = [
+        'name' => 'required|between:2,255',
+    ];
+
+    public function mount()
+    {
+        $user = Auth::user();
+
+        $this->user = $user;
+        $this->name = $user->name;
+        $this->email = $user->email;
+    }
+
+    /**
+     * Enable input for edit name.
+     */
+    public function editName()
+    {
+        $this->toggleName = true;
+        $this->dispatch('name');
+    }
+
+    /**
+     * Update user.
+     */
+    public function updateName()
+    {
+        $this->validateOnly('name');
+
+        $this->user->update([
+            'name' => $this->name,
+        ]);
+
+        $this->reset('toggleName');
+
+        session()->flash('message', 'Has been updated name successfull.');
+
+        $this->dispatch('update-name', name: $this->name);
+    }
+
     public function render()
     {
         return view('livewire.update-user');
--- resources/views/livewire/update-user.blade.php
+++ resources/views/livewire/update-user.blade.php
@@ -1,29 +1,48 @@
-<form class="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+<div class="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
     <div class="px-4 py-6 sm:p-8">
         <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div class="sm:col-span-2">
-                <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First
-                    name</label>
-                <div class="mt-2">
-                    <input type="text" name="first-name" id="first-name" autocomplete="given-name"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                </div>
+            <div class="sm:col-span-3">
+                <form wire:submit.prevent="updateName">
+                    <label for="name" class="block text-sm font-medium leading-6 text-gray-900 flex items-center justify-between">
+                        <span class="text-gray-500">Name</span>
+                        <a
+                            href="#"
+                            wire:click.prevent="editName"
+                            class="{{ $toggleName ? 'hidden' : ''}} text-teal-500 hover:text-blue-700 transition duration-300 ease-in-out py-1 px-3 sm:mr-0 -mr-3"
+                        >
+                            <svg class="flex-shrink-0 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                <path fill="currentColor" d="M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z"></path>
+                            </svg>
+                        </a>
+                        @if ($toggleName)
+                            <button type="submit" class="text-indigo-500 hover:text-indigo-700 transition duration-300 ease-in-out text-xs py-1 px-3 -mb-px sm:mr-0 -mr-3">
+                                Guardar
+                            </button>
+                        @endif
+                    </label>
+                    <div class="mt-2">
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            autocomplete="given-name"
+                            wire:model.defer="name"
+                            class="{{ $toggleName ? 'active' : '' }} block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </form>
             </div>
 
-            <div class="sm:col-span-2">
-                <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last
-                    name</label>
+            <div class="sm:col-span-3">
+                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">
+                    Email address
+                </label>
                 <div class="mt-2">
-                    <input type="text" name="last-name" id="last-name" autocomplete="family-name"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                </div>
-            </div>
-
-            <div class="sm:col-span-2">
-                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email
-                    address</label>
-                <div class="mt-2">
-                    <input id="email" name="email" type="email" autocomplete="email"
+                    <input 
+                        id="email"
+                        name="email"
+                        type="email"
+                        autocomplete="email"
+                        wire:model.defer="email"
                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
@@ -36,10 +55,6 @@
                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
-
-
-
-
 
             <div class="sm:col-span-2">
                 <label for="birthday"
@@ -99,4 +114,13 @@
             </ul>
         </div>
     </div>
-</form>
+</div>
+
+<script>
+    window.addEventListener('livewire:init', () => {
+        Livewire.on('name', (event) => {
+            console.log('Event name');
+            document.getElementById('name').focus();
+        })
+    })
+</script>
