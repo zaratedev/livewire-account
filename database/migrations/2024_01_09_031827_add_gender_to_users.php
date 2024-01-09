b344c86cbd0706cf718ec6ad242f58bfe39e68c8
Jonathan Zarate <zaratedev@gmail.com>
2024-01-09 12:33:21 -0600

    Fixed nullable gender

--- database/migrations/2024_01_09_031827_add_gender_to_users.php
+++ database/migrations/2024_01_09_031827_add_gender_to_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('gender', 10);
+            $table->string('gender', 10)->nullable();
         });
     }
 
